84ac0cdd5e6f50a90fe9e02c0415e6c0ff441543
ljhgwb <ljhgwb@github.com>
2026-08-14 19:07:18 +0800

    7

--- src/main/java/com/chongwu/system/ChongwuApplication.java
+++ src/main/java/com/chongwu/system/ChongwuApplication.java
@@ -10,7 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @MapperScan("com.chongwu.system.mapper")
 public class ChongwuApplication {
-//ss
+//ss66
     public static void main(String[] args) {
         SpringApplication.run(ChongwuApplication.class, args);
     }
