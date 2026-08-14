38db6fba142fa9085c18680caacfb487d4e9999f
ljhgwb <ljhgwb@github.com>
2026-08-14 18:14:44 +0800

    2

---
+++ src/main/java/com/chongwu/system/service/OrderService.java
@@ -0,0 +1,10 @@
+package com.chongwu.system.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.chongwu.system.entity.Order;
+
+/**
+ * 订单服务接口
+ */
+public interface OrderService extends IService<Order> {
+}
