ea95b338985c12a78f6d1c0f90a3c87d7421a32b
ljhgwb <ljhgwb@github.com>
2026-08-14 18:08:37 +0800

    1

---
+++ src/main/java/com/chongwu/system/ChongwuApplication.java
@@ -0,0 +1,17 @@
+package com.chongwu.system;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 宠物寄养服务管理系统启动类
+ */
+@SpringBootApplication
+@MapperScan("com.chongwu.system.mapper")
+public class ChongwuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChongwuApplication.class, args);
+    }
+}
