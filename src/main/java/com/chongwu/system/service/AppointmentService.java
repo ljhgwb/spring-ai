051883c24e7866a02f6a9ec84b794158b68e9b97
ljhgwb <ljhgwb@github.com>
2026-08-14 18:12:44 +0800

    2

---
+++ src/main/java/com/chongwu/system/service/AppointmentService.java
@@ -0,0 +1,10 @@
+package com.chongwu.system.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.chongwu.system.entity.Appointment;
+
+/**
+ * 寄养预约服务接口
+ */
+public interface AppointmentService extends IService<Appointment> {
+}
